51bade874f0f68b4e0f24f0ecd564456a145294a
Josehower <josehowerupn@gmail.com>
2020-09-16 14:06:13 +0200

    Implement all the the requirments to send to karl rewiew

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import React, { useState, useEffect } from 'react';
+
+function LocationWeatherInfoBlock({ getWeather, onload = false }) {
+  const [weatherInfo, setWeatherInfo] = useState({});
+  const [city, setCity] = useState('');
+
+  function getInfo(cityName) {
+    getWeather(cityName)
+      .then((response) => response.json())
+      .then((res) => {
+        setWeatherInfo(res);
+      });
+  }
+
+  useEffect(() => {
+    if (onload) {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        console.log(position, 'frist time');
+        fetch(
+          `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=bfa02c38b1011a52dd60675378b19bc2&units=metric`,
+        )
+          .then((response) => response.json())
+          .then((res) => setWeatherInfo(res));
+      });
+    }
+  }, [onload]);
+
+  return (
+    <div style={{ display: 'inline-block', margin: '10px' }}>
+      <img
+        src={`http://openweathermap.org/img/wn/${weatherInfo?.weather?.[0]?.icon}@2x.png`}
+        alt=""
+      />
+      <p>Location &#8594; {weatherInfo?.name}</p>
+      <p>Temperature &#8594; {weatherInfo?.main?.temp}</p>
+      <p>Weather &#8594; {weatherInfo?.weather?.[0]?.description}</p>
+      <label htmlFor="city">your city</label>
+      <input
+        value={city}
+        onChange={(e) => setCity(e.currentTarget.value)}
+        type="text"
+        name="city"
+      />
+      <button onClick={() => getInfo(city)}>get your weather</button>
+    </div>
+  );
+}
+
+function App() {
+  const [extraLocations, setExtraLocations] = useState([]);
+
+  function addLocation() {
+    const actualState = extraLocations;
+    const locationTemplate = (
+      <LocationWeatherInfoBlock key={new Date()} getWeather={getWeather} />
+    );
+    setExtraLocations([...actualState, locationTemplate]);
+  }
+
+  function getWeather(cityName) {
+    return fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=bfa02c38b1011a52dd60675378b19bc2&units=metric`,
+    );
+  }
+
+  return (
+    <div>
+      <LocationWeatherInfoBlock getWeather={getWeather} onload="true" />
+      {extraLocations}
+      <button onClick={addLocation}>add Location</button>
+    </div>
+  );
+}
+
+export default App;
